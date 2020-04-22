50205a70554979afc0f2db8efef4a1f2d74af92f
Dmitry Ledentsov <dmlled@yahoo.com>
2020-04-22 15:19:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleDump;
+
+namespace dotnet_core_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello World!".Dump();
+            new []{1,2,3}.Dump();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnet_core_playground
+ 
